4ea63ba00b7c2cc4ca9434aad64456a7c4f1598f
unknown <salim.rabbaoui@esprit.tn>
2023-11-17 08:08:16 +0100

    implement interfaces

---
+++ app/src/main/java/tn/esprit/pdm_draft/PackageDetailsActivity.kt
@@ -0,0 +1,11 @@
+package tn.esprit.pdm_draft
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PackageDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_package_details)
+    }
+}
---
+++ app/src/main/java/tn/esprit/pdm_draft/ReceiverDetailActivity.kt
@@ -0,0 +1,11 @@
+package tn.esprit.pdm_draft
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReceiverDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_receiver_detail)
+    }
+}
